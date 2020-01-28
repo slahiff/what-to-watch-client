f863cf433d4019c1c2e07414376c3793e6922c29
slahiff <sean243446@gmail.com>
2020-01-28 13:35:01 -0500

    Slight change to Nav bar and page title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,9 +23,9 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar bg="info" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      What to Watch
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
