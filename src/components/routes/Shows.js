e1c8ceeb0d00746d6e86ed8957f442c792178bf0
slahiff <sean243446@gmail.com>
2020-02-12 04:54:08 -0500

    Added embedded trailer to shows route

--- src/components/routes/Shows.js
+++ src/components/routes/Shows.js
@@ -15,22 +15,29 @@ const Shows = props => {
   }, [])
 
   const showsArr = shows.map(show => (
-    <div key={show.id} className="show-div">
-      <Link to={`/shows/${show.id}`}>
-        <h6>
-          <b>{`${show.title} (Season ${show.season_number})`}</b>
-        </h6>
-      </Link>
-      <ul>
-        <li><b>Network/Service</b>: {show.network}</li>
-        <li><b>Number of Episodes</b>: {show.total_episodes}</li>
-        <li><b>Date Released</b>: {show.release_date}</li>
-        <br/>
-        <b>User Reviews </b>
-        <Link to={`/shows/${show.id}`}>
-          {`(${show.reviews.length})`}
+    <div key={show.id} className="card-deck">
+      <div className="show-div card">
+        <Link to={`/shows/${show.id}`} className="card-header">
+          <h6>
+            <b>{`${show.title} (Season ${show.season_number})`}</b>
+          </h6>
         </Link>
-      </ul>
+        <ul>
+          <li><b>Network/Service</b>: {show.network}</li>
+          <li><b>Number of Episodes</b>: {show.total_episodes}</li>
+          <li><b>Date Released</b>: {show.release_date}</li>
+        </ul>
+        <br/>
+        <div className="card-footer">
+          <b>User Reviews </b>
+          <Link to={`/shows/${show.id}`}>
+            {`(${show.reviews.length})`}
+          </Link>
+        </div>
+      </div>
+      <div className="card">
+        <iframe className="shows-iframe" src={`https://www.youtube.com/embed/${show.trailer_url.slice(-11)}?hd=1&amp;modestbranding=1`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
     </div>
   ))
 
